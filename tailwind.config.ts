72ef7d152d0d34f1bdafaebe2a789eea9328c5e2
heahl <hendrik712.a@gmail.com>
2024-05-11 20:24:49 +0200

    last commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,13 @@ export default {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
+      colors: {
+        'primary-green': 'hsl(75, 94%, 57%)',
+        'white': 'hsl(0, 0%, 100%)',
+        'grey': 'hsl(0, 0%, 20%)',
+        'dark-grey': 'hsl(0, 0%, 12%)',
+        'off-black': 'hsl(0, 0%, 8%)',
+      },
       fontFamily: {
         sans: ["var(--font-geist-sans)", ...fontFamily.sans],
       },
